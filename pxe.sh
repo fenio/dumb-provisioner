570ccdeb90dedf2d7f3e24d426154943097b1eb3
Bartosz Fenski <fenio@debian.org>
2023-01-22 20:43:05 +0100

    pxe fixes

--- pxe.sh
+++ pxe.sh
@@ -3,4 +3,4 @@ wget -nc https://deb.debian.org/debian/dists/bullseye/main/installer-amd64/curre
 tar zxf netboot/netboot.tar.gz -C netboot
 cp netboot/debian-installer/amd64/linux pxe/
 cp netboot/debian-installer/amd64/initrd.gz pxe/
-grub-mkimage -d /usr/lib/grub/x86_64-efi/ -O x86_64-efi -o ./pxe/bootx64.efi -p ‘(pxe)/grub’ efinet tftp
+grub-mkimage -d /usr/lib/grub/x86_64-efi/ -O x86_64-efi -o ./pxe/bootx64.efi -p ‘\(pxe\)/grub’ efinet tftp
