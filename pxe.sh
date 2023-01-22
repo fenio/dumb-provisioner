07cc3a92d7801b55d6326323648da912df84ed42
Bartosz Fenski <fenio@debian.org>
2023-01-22 20:40:51 +0100

    pxe script

---
+++ pxe.sh
@@ -0,0 +1,6 @@
+mkdir -p pxe netboot
+wget -nc https://deb.debian.org/debian/dists/bullseye/main/installer-amd64/current/images/netboot/netboot.tar.gz -O netboot/netboot.tar.gz
+tar zxf netboot/netboot.tar.gz -C netboot
+cp netboot/debian-installer/amd64/linux pxe/
+cp netboot/debian-installer/amd64/initrd.gz pxe/
+grub-mkimage -d /usr/lib/grub/x86_64-efi/ -O x86_64-efi -o ./pxe/bootx64.efi -p ‘(pxe)/grub’ efinet tftp
