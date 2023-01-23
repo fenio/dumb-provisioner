7aab05762f61c6b0c07592530280e325115882ff
Bartosz Fenski <fenio@debian.org>
2023-01-23 06:44:32 +0100

    cleanup

--- pxe.sh
+++ pxe.sh
@@ -4,3 +4,4 @@ tar zxf netboot/netboot.tar.gz -C netboot
 cp netboot/debian-installer/amd64/linux pxe/
 cp netboot/debian-installer/amd64/initrd.gz pxe/
 grub-mkimage -d /usr/lib/grub/x86_64-efi/ -O x86_64-efi -o ./pxe/bootx64.efi -p ‘\(pxe\)/grub’ efinet tftp
+cp -r netboot/debian-installer/amd64/grub/x86_64-efi pxe/grub/
