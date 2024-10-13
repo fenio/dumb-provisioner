aeba94853cb26342d7eda64a440c90056a59b029
Bartosz Fenski <fenio@debian.org>
2024-10-13 19:21:23 +0200

    migration to new hw

--- pxe.sh
+++ pxe.sh
@@ -3,4 +3,4 @@ wget -nc https://deb.debian.org/debian/dists/bookworm/main/installer-amd64/curre
 tar zxf netboot/netboot.tar.gz -C netboot
 cp netboot/debian-installer/amd64/linux pxe/
 cp netboot/debian-installer/amd64/initrd.gz pxe/
-grub-mkstandalone -d /usr/lib/grub/x86_64-efi/ -O x86_64-efi –fonts=”unicode” -o ./pxe/bootx64.efi "boot/grub/grub.cfg=./grub.cfg"
+grub-mkstandalone --compress=xz -d /usr/lib/grub/x86_64-efi/ -O x86_64-efi –fonts=”unicode” -o ./pxe/bootx64.efi "boot/grub/grub.cfg=./grub.cfg"
